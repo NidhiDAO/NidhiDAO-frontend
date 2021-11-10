c0f6cab43e87d47b7571fb0014415526b6ae7394
Kurma <kurma@nidhidao.finance>
2021-11-10 17:14:53 +0200

    feat: not found page

--- src/views/404/NotFound.jsx
+++ src/views/404/NotFound.jsx
@@ -1,15 +1,10 @@
-import OlympusLogo from "../../assets/Olympus Logo.svg";
 import "./notfound.scss";
 
 export default function NotFound() {
   return (
     <div id="not-found">
       <div className="not-found-header">
-        <a href="https://olympusdao.finance" target="_blank">
-          <img className="branding-header-icon" src={OlympusLogo} alt="OlympusDAO" />
-        </a>
-
-        <h4>Page not found</h4>
+        <h1>Page not found</h1>
       </div>
     </div>
   );
