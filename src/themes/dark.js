fa849e45f10d81c997fef52f98611ea3ece2e5c2
Kurma <kurma@nidhidao.finance>
2021-11-10 16:55:32 +0200

    feat: global styles

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -5,28 +5,39 @@ import commonSettings, { handleBackdropFilter } from "./global.js";
 // TODO: Break repeated use color values out into list of consts declared here
 // then set the values in darkTheme using the global color variables
 
+const colors = {
+  gold: {
+    500: "#FFBC45",
+    600: "#F0A72C",
+  },
+  common: {
+    white: "#FCFCFC",
+    black: "",
+  },
+  dark: {
+    700: "#344750",
+    800: "#232E33",
+    900: "#182328",
+  },
+};
+
 const darkTheme = {
-  color: "#FCFCFC",
-  gold: "#F8CC82",
+  color: colors.common.white,
+  gold: colors.gold[500],
   gray: "#A3A3A3",
-  textHighlightColor: "#F4D092",
-  backgroundColor: "rgba(8, 15, 53, 1)",
+  textHighlightColor: colors.gold[500],
+  backgroundColor: colors.dark[800],
   background: `
-    linear-gradient(180deg, rgba(8, 15, 53, 0), rgba(0, 0, 10, 0.9)),
-    linear-gradient(333deg, rgba(153, 207, 255, 0.2), rgba(180, 255, 217, 0.08)),
-    radial-gradient(circle at 77% 89%, rgba(125, 163, 169, 0.8), rgba(125, 163, 169, 0) 50%),
-    radial-gradient(circle at 15% 95%, rgba(125, 163, 169, 0.8), rgba(125, 163, 169, 0) 43%),
-    radial-gradient(circle at 65% 23%, rgba(137, 151, 119, 0.4), rgba(137, 151, 119, 0) 70%),
-    radial-gradient(circle at 10% 0%, rgba(187, 211, 204, 0.33), rgba(187,211,204,0) 35%),
-    radial-gradient(circle at 11% 100%, rgba(131, 165, 203, 0.3), rgba(131, 165, 203, 0) 30%)
-    `,
-  paperBg: "rgba(54, 56, 64, 0.4)",
+    radial-gradient(circle at 44% 35%, rgba(255, 109, 0, .25), rgba(255, 109, 0, 0) 27%),
+    radial-gradient(circle at 75% 59%, rgba(0, 209, 197, .25), rgba(0, 209, 197, 0) 33%)
+  `,
+  paperBg: `${colors.dark[900]}40`,
   modalBg: "#24242699",
-  popoverBg: "rgba(54, 56, 64, 0.99)",
-  menuBg: handleBackdropFilter("rgba(54, 56, 64, 0.5)"),
+  popoverBg: colors.dark[900],
+  menuBg: "#36384080",
   backdropBg: "rgba(54, 56, 64, 0.5)",
-  largeTextColor: "#F4D092",
-  activeLinkColor: "#F5DDB4",
+  largeTextColor: colors.gold[500],
+  activeLinkColor: colors.gold[500],
   activeLinkSvgColor:
     "brightness(0) saturate(100%) invert(84%) sepia(49%) saturate(307%) hue-rotate(326deg) brightness(106%) contrast(92%)",
   primaryButtonColor: "#333333",
@@ -37,8 +48,12 @@ const darkTheme = {
   outlinedPrimaryButtonHoverColor: "#333333",
   outlinedSecondaryButtonHoverBG: "transparent",
   outlinedSecondaryButtonHoverColor: "#F8CC82", //gold
-  containedSecondaryButtonHoverBG: "rgba(255, 255, 255, 0.15)",
+  containedSecondaryButtonBG: colors.dark[900],
+  containedSecondaryButtonHoverBG: colors.dark[700],
   graphStrokeColor: "rgba(255, 255, 255, .1)",
+
+  containedIconButton: colors.common.white,
+  containedIconButtonHover: colors.gold[500],
 };
 
 export const dark = responsiveFontSizes(
@@ -93,11 +108,25 @@ export const dark = responsiveFontSizes(
         MuiPaper: {
           root: {
             backgroundColor: darkTheme.paperBg,
+            backdropFilter: "blur(200px)",
+            background: `radial-gradient(circle at 28% 13%, rgba(255, 109, 0, .06), rgba(255, 109, 0, 0) 24%)`,
             "&.ohm-card": {
+              background: "rgba(255, 255, 255, 0.05)",
+              border: "0.5px solid rgba(255, 255, 255, 0.2)",
+              boxShadow: "0px 20px 20px rgba(0, 0, 0, 0.05)",
               backgroundColor: darkTheme.paperBg,
+              backdropFilter: "blur(40px)",
+            },
+            "&.bond-card": {
+              background: colors.dark[900],
+              border: "0.5px solid rgba(255, 255, 255, 0.2)",
+              boxShadow: "0px 20px 20px rgba(0, 0, 0, 0.05)",
+              // backgroundColor: darkTheme.paperBg,
+              // backdropFilter: "blur(40px)",
             },
             "&.ohm-modal": {
-              backgroundColor: darkTheme.modalBg,
+              background: colors.dark[900],
+              // backgroundColor: darkTheme.modalBg,
             },
             "&.ohm-menu": {
               backgroundColor: darkTheme.menuBg,
@@ -118,16 +147,28 @@ export const dark = responsiveFontSizes(
         MuiLink: {
           root: {
             color: darkTheme.color,
+            "& #bond-icon, & #doc-icon, & #dashboard-icon, & #settings-icon": {
+              fill: "none",
+              stroke: darkTheme.color,
+            },
             "&:hover": {
+              "&:hover #bond-icon, &:hover #doc-icon, & #dashboard-icon, & #settings-icon": {
+                fill: "none",
+                stroke: darkTheme.textHighlightColor,
+              },
               color: darkTheme.textHighlightColor,
               textDecoration: "none",
               "&.active": {
-                color: darkTheme.color,
+                color: darkTheme.activeLinkColor,
               },
             },
             "&.active": {
-              color: darkTheme.color,
-              textDecoration: "underline",
+              color: darkTheme.activeLinkColor,
+              textDecoration: "none",
+              "& #dashboard-icon, & #bond-icon": {
+                fill: darkTheme.textHighlightColor,
+                stroke: darkTheme.textHighlightColor,
+              },
             },
           },
         },
@@ -159,7 +200,13 @@ export const dark = responsiveFontSizes(
         },
         PrivateTabIndicator: {
           colorPrimary: {
+            "&$selected": {
+              color: darkTheme.gold,
+            },
             backgroundColor: darkTheme.gold,
+            // "&.active": {
+            color: darkTheme.activeLinkColor,
+            // },
           },
         },
         MuiToggleButton: {
@@ -207,8 +254,8 @@ export const dark = responsiveFontSizes(
             },
           },
           containedSecondary: {
-            backgroundColor: darkTheme.paperBg,
-            color: darkTheme.color,
+            border: "0.5px solid rgba(255, 255, 255, 0.2)",
+            backgroundColor: darkTheme.containedSecondaryButtonBG,
             "&:hover": {
               backgroundColor: `${darkTheme.containedSecondaryButtonHoverBG} !important`,
             },
@@ -267,6 +314,22 @@ export const dark = responsiveFontSizes(
             color: darkTheme.color,
             "&:hover": {
               color: darkTheme.textHighlightColor,
+            },
+          },
+        },
+        MuiIconButton: {
+          root: {
+            color: darkTheme.containedIconButton,
+            "& #settings-icon": {
+              fill: "none",
+              stroke: darkTheme.containedIconButton,
+            },
+            "&:hover": {
+              color: darkTheme.containedIconButtonHover,
+            },
+            "&:hover #settings-icon": {
+              fill: "none",
+              stroke: darkTheme.containedIconButtonHover,
             },
           },
         },
